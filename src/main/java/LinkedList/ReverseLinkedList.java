431f0b1e74d785df42635a20297ee691150ab962
Tanner-Wills <tannerwills1@gmail.com>
2022-09-05 13:10:41 -0400

    added more problems

--- src/main/java/LinkedList/ReverseLinkedList.java
+++ src/main/java/LinkedList/ReverseLinkedList.java
@@ -1,17 +1,15 @@
 package LinkedList;
 
 public class ReverseLinkedList {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
-    /*
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
@@ -22,6 +20,31 @@ public class ReverseLinkedList {
             curr = nextTemp;
         }
         return prev;
+    }
+
+    /*
+    // iterative solution
+    ListNode newHead = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
+
+    public ListNode reverseList(ListNode head) {
+        // recursive solution
+        return reverseListInt(head, null);
+    }
+
+    private ListNode reverseListInt(ListNode head, ListNode newHead) {
+        if (head == null)
+            return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListInt(next, head);
     }
      */
 }

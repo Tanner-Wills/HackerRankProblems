f336ab61c25581cf59665f1fe19d3ce305978f76
Tanner-Wills <tannerwills1@gmail.com>
2022-09-05 21:49:58 -0400

    added more problems

---
+++ src/main/java/TwoPointer/SquaresOfASortedArray.java
@@ -0,0 +1,32 @@
+package TwoPointer;
+
+public class SquaresOfASortedArray {
+    /*
+    Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+     */
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
+    }
+}

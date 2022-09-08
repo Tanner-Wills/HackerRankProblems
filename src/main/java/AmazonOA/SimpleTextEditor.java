d2bf3da72230985da229e05f1ae0085bd3e1733d
Tanner-Wills <tannerwills1@gmail.com>
2022-09-08 09:53:10 -0400

    added more problems

---
+++ src/main/java/AmazonOA/SimpleTextEditor.java
@@ -0,0 +1,35 @@
+package AmazonOA;
+
+public class SimpleTextEditor {
+
+    /*
+    # Enter your code here. Read input from STDIN. Print output to STDOUT
+mainString = ""
+historyString  = []
+
+t = int(input())
+for i in range(t):
+    inputString = input().strip().split()
+    command = int(inputString[0])
+
+    if command == 1:
+        historyString.append(mainString)
+        mainString+=inputString[1]
+
+
+    if command == 2:
+        historyString.append(mainString)
+        mainString = mainString[0:(len(mainString)-int(inputString[1]))]
+
+
+    if command == 3:
+        index = int(inputString[1])-1
+        print(mainString[index])
+
+
+    if command == 4:
+        mainString = historyString[-1]
+        historyString.pop()
+
+     */
+}
